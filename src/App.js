5fc10ee348489a9aed01be11b58fcb5f01460615
Daexaf <ihsan64.mi@gmail.com>
2023-07-07 20:40:22 +0700

    haha

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useEffect } from "react";
 import Home from "./pages/Home";
 import "../src/App.css";
