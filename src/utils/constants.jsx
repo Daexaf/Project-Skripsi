9090368acd89b7cd0d7fa7724a2371914106e196
Daexaf <ihsan64.mi@gmail.com>
2023-07-07 20:04:28 +0700

    salah alamat

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,4 +1,4 @@
-export const API_URL2 = "http://server-skripsi-2-production.up.railway.app/";
+export const API_URL2 = "https://server-skripsi-2-production.up.railway.app/";
 
 export const API_URL3 = "server-skripsi-2-production.up.railway.app";
 // http://localhost:4000/
