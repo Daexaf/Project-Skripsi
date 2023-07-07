b4bf10e717207901753b9c10226c3dbf526f43a3
Daexaf <ihsan64.mi@gmail.com>
2023-07-07 20:01:44 +0700

    ganti alamat palsu

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,1 +1,4 @@
-export const API_URL2 = "http://localhost:4000/";
+export const API_URL2 = "http://server-skripsi-2-production.up.railway.app/";
+
+export const API_URL3 = "server-skripsi-2-production.up.railway.app";
+// http://localhost:4000/
