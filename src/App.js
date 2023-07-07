47fa80da53d0d76e5f48b44a219f5714b926854d
Daexaf <ihsan64.mi@gmail.com>
2023-07-07 20:33:23 +0700

    commit is my routine

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useEffect } from "react";
 import Home from "./pages/Home";
 import "../src/App.css";
