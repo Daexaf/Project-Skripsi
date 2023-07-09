b682ea03705cef4c74c2c09b9009d93a9026c66f
Daexaf <ihsan64.mi@gmail.com>
2023-07-09 11:51:31 +0700

    try midtrans

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { API_URL2 } from "../utils/constants";
+import { Helmet } from "react-helmet";
 
 const PaymentForm = () => {
   const { id } = useParams();
@@ -9,65 +10,77 @@ const PaymentForm = () => {
   const { dataKeranjang, setDataKeranjang } = useState();
 
   // useEffect(() => {
-  //   axios
-  //     .get(API_URL2 + `table/${id}`)
-  //     .then((res) => {
-  //       console.log(res.data.data[0]);
-  //       setDataUser(res.data.data[0]);
-  //     })
-  //     .catch((error) => {
-  //       console.log("Error yaa ", error);
-  //     });
+  //   // You can also change below url value to any script url you wish to load,
+  //   // for example this is snap.js for Sandbox Env (Note: remove `.sandbox` from url if you want to use production version)
+  //   const midtransScriptUrl = "https://app.midtrans.com/snap/snap.js";
 
-  //   axios
-  //     .get(API_URL2 + `keranjangs?id_tables=${id}`)
-  //     .then((res) => {
-  //       console.log(res.data.data, "ini resnya");
-  //       setDataKeranjang(res.data.data);
-  //     })
-  //     .catch((error) => {
-  //       console.log("Error yaa ", error);
-  //     });
-  // }, [id]);
+  //   let scriptTag = document.createElement("script");
+  //   scriptTag.src = midtransScriptUrl;
 
-  //SAMPLE REQUEST START HERE
+  //   // Optional: set script attribute, for example snap.js have data-client-key attribute
+  //   // (change the value according to your client-key)
+  //   const myMidtransClientKey = "SB-Mid-client-3x1AxTOk89GduFm7";
+  //   scriptTag.setAttribute(
+  //     "SB-Mid-client-3x1AxTOk89GduFm7",
+  //     myMidtransClientKey
+  //   );
 
-  const midtransClient = require("midtrans-client");
-  // Create Snap API instance
-  let snap = new midtransClient.Snap({
-    // Set to true if you want Production Environment (accept real transaction).
-    isProduction: false,
-    serverKey: "SB-Mid-server-o8AeFK89-NJwJwf6QY1RMTgu",
-  });
+  //   document.body.appendChild(scriptTag);
 
-  let parameter = {
-    transaction_details: {
-      order_id: "YOUR-ORDERID-123456",
-      gross_amount: 10000,
-    },
-    credit_card: {
-      secure: true,
-    },
-    customer_details: {
-      first_name: "budi",
-      last_name: "pratama",
-      email: "budi.pra@example.com",
-      phone: "08111222333",
-    },
+  //   return () => {
+  //     document.body.removeChild(scriptTag);
+  //   };
+  // }, []);
+
+  const handlePayButton = (e) => {
+    window.snap.pay("ea70efa7-addc-4b70-9dc0-bef2f2c4f28f", {
+      onSuccess: function (result) {
+        /* You may add your own implementation here */
+        alert("payment success!");
+        console.log(result);
+      },
+      onPending: function (result) {
+        /* You may add your own implementation here */
+        alert("wating your payment!");
+        console.log(result);
+      },
+      onError: function (result) {
+        /* You may add your own implementation here */
+        alert("payment failed!");
+        console.log(result);
+      },
+      onClose: function () {
+        /* You may add your own implementation here */
+        alert("you closed the popup without finishing the payment");
+      },
+    });
   };
 
-  snap.createTransaction(parameter).then((transaction) => {
-    // transaction token
-    let transactionToken = transaction.token;
-    console.log("transactionToken:", transactionToken);
-  });
+  // Then somewhere else on your React component, `window.snap` global object will be available to use
+  // e.g. you can then call `window.snap.pay( ... )` function.
 
   return (
-    <div>
-      {/* Formulir pembayaran */}
-      {/* <button onClick={handlePayment} disabled={loading}></button> */}
-      <p>tes</p>
-    </div>
+    <>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <!-- @TODO: replace SET_YOUR_CLIENT_KEY_HERE with your client key --> */}
+        <script
+          type="text/javascript"
+          src="https://app.sandbox.midtrans.com/snap/snap.js"
+          data-client-key="SB-Mid-client-3x1AxTOk89GduFm7"
+        ></script>
+        {/* <!-- Note: replace with src="https://app.midtrans.com/snap/snap.js" for Production environment --> */}
+      </Helmet>
+
+      <button
+        id="pay-button"
+        onClick={(e) => {
+          handlePayButton(e);
+        }}
+      >
+        Pay!
+      </button>
+    </>
   );
 };
 
--- src/pages/Sukses.jsx
+++ src/pages/Sukses.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import "./sukses.css";
 import { API_URL2 } from "../utils/constants";
 import numberWithCommas from "../utils/utils";
+import { Helmet } from "react-helmet";
 
 const Sukses = () => {
   const { id } = useParams();
@@ -53,78 +54,118 @@ const Sukses = () => {
 
   console.log(menu, "ini data menu");
 
-  return (
-    <div className=" items-center flex justify-center h-full">
-      <Card border="primary" style={{ width: "30rem" }} className="mt-10 mb-10">
-        <Card.Body>
-          <Card.Header className="text-center">
-            Restoran Sop Duren 97
-          </Card.Header>
-          <Card.Title className="mt-2">
-            Nama Pemesan: {tableDetail?.name}
-          </Card.Title>
-          <Card.Title className="mt-2">
-            pesanan meja #{tableDetail?.table_name}
-          </Card.Title>
-          <Card.Subtitle className=" text-muted">
-            {tableDetail?.time_start}
-          </Card.Subtitle>
-          <Card.Text className="text-center text-black">
-            Menu yang dipesan
-          </Card.Text>
-          <ListGroup className="list-group-flush">
-            <ListGroup as="ol" numbered>
-              {menu &&
-                menu.map((item) => (
-                  <ListGroup.Item
-                    key={item.id_keranjangs}
-                    as="li"
-                    className="d-flex justify-content-between align-items-start"
-                  >
-                    <div className="ms-2 me-auto">
-                      <div className="fw-bold">{item.product[0].name}</div>
-                      {item.keterangan}
-                      <Badge
-                        bg="primary"
-                        className="mt-3 mr-3 text-center"
-                        style={{ width: "20px", height: "20px" }}
-                      >
-                        {item.jumlah}
-                      </Badge>
-                      <p className="text-black">
-                        Rp. {numberWithCommas(item.total_harga)}
-                      </p>
-                    </div>
-                  </ListGroup.Item>
-                ))}
-              <Card.Title className="mt-2 ml-3">
-                Total Harga: Rp. {numberWithCommas(totalBayar)}
-              </Card.Title>
-            </ListGroup>
-          </ListGroup>
-        </Card.Body>
+  const handlePayButton = (e) => {
+    window.snap.pay("ffa66f4d-937b-472d-a1d2-77aac7dd6452", {
+      onSuccess: function (result) {
+        /* You may add your own implementation here */
+        alert("payment success!");
+        console.log(result);
+      },
+      onPending: function (result) {
+        /* You may add your own implementation here */
+        alert("wating your payment!");
+        console.log(result);
+      },
+      onError: function (result) {
+        /* You may add your own implementation here */
+        alert("payment failed!");
+        console.log(result);
+      },
+      onClose: function () {
+        /* You may add your own implementation here */
+        alert("you closed the popup without finishing the payment");
+      },
+    });
+  };
 
-        <Card.Body className="text-center">
-          <Button
-            variant="primary"
-            className="mr-5"
-            onClick={() => {
-              navigate(`/Order/${id}`);
-            }}
-          >
-            Kembali
-          </Button>{" "}
-          <Button
-            variant="primary"
-            onClick={() => {
-              navigate(`/feedback/${id}`);
-            }}
-          >
-            Bayar
-          </Button>{" "}
-        </Card.Body>
-      </Card>
-    </div>
+  return (
+    <>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <!-- @TODO: replace SET_YOUR_CLIENT_KEY_HERE with your client key --> */}
+        <script
+          type="text/javascript"
+          src="https://app.sandbox.midtrans.com/snap/snap.js"
+          data-client-key="SB-Mid-client-3x1AxTOk89GduFm7"
+        ></script>
+        {/* <!-- Note: replace with src="https://app.midtrans.com/snap/snap.js" for Production environment --> */}
+      </Helmet>
+      <div className=" items-center flex justify-center h-full">
+        <Card
+          border="primary"
+          style={{ width: "30rem" }}
+          className="mt-10 mb-10"
+        >
+          <Card.Body>
+            <Card.Header className="text-center">
+              Restoran Sop Duren 97
+            </Card.Header>
+            <Card.Title className="mt-2">
+              Nama Pemesan: {tableDetail?.name}
+            </Card.Title>
+            <Card.Title className="mt-2">
+              pesanan meja #{tableDetail?.table_name}
+            </Card.Title>
+            <Card.Subtitle className=" text-muted">
+              {tableDetail?.time_start}
+            </Card.Subtitle>
+            <Card.Text className="text-center text-black">
+              Menu yang dipesan
+            </Card.Text>
+            <ListGroup className="list-group-flush">
+              <ListGroup as="ol" numbered>
+                {menu &&
+                  menu.map((item) => (
+                    <ListGroup.Item
+                      key={item.id_keranjangs}
+                      as="li"
+                      className="d-flex justify-content-between align-items-start"
+                    >
+                      <div className="ms-2 me-auto">
+                        <div className="fw-bold">{item.product[0].name}</div>
+                        {item.keterangan}
+                        <Badge
+                          bg="primary"
+                          className="mt-3 mr-3 text-center"
+                          style={{ width: "20px", height: "20px" }}
+                        >
+                          {item.jumlah}
+                        </Badge>
+                        <p className="text-black">
+                          Rp. {numberWithCommas(item.total_harga)}
+                        </p>
+                      </div>
+                    </ListGroup.Item>
+                  ))}
+                <Card.Title className="mt-2 ml-3">
+                  Total Harga: Rp. {numberWithCommas(totalBayar)}
+                </Card.Title>
+              </ListGroup>
+            </ListGroup>
+          </Card.Body>
+
+          <Card.Body className="text-center">
+            <Button
+              variant="primary"
+              className="mr-5"
+              onClick={() => {
+                navigate(`/Order/${id}`);
+              }}
+            >
+              Kembali
+            </Button>{" "}
+            <Button
+              variant="primary"
+              onClick={() => {
+                handlePayButton();
+              }}
+            >
+              Bayar
+            </Button>{" "}
+          </Card.Body>
+        </Card>
+      </div>
+    </>
   );
 };
 
