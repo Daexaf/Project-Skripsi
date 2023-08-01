815027573fb774d00de4d4e683db381651f7807d
Daexaf <ihsan64.mi@gmail.com>
2023-08-02 00:05:01 +0700

    changing constat data

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,4 +1,4 @@
-// export const API_URL2 = "https://server-skripsi-2-production.up.railway.app/";
-export const API_URL2 = "http://localhost:4000/";
+export const API_URL2 = "https://server-skripsi-2-production.up.railway.app/";
+// export const API_URL2 = "http://localhost:4000/";
 export const API_URL3 = "server-skripsi-2-production.up.railway.app";
 export const API_URL4 = "http://localhost:4000";
